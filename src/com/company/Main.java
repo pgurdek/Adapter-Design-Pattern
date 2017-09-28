f7c411d18aca1c688d231d8d16d848de2b720966
Piotr <pgurdek@gmail.com>
2017-09-28 12:44:39 +0200

    Remove unnecessary file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        EmployeeClient client = new EmployeeClient();
+
+        List<Employee> employees = client.getEmployeeList();
+
+        System.out.println(employees);
     }
 }
